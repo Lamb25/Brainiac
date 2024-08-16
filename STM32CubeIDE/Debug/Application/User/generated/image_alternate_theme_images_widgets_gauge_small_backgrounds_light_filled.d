a27018c9deee8b4c995665bf1bb293094293ec19
Cesar Cordero <cordero25cesar@gmail.com>
2024-08-16 06:54:43 -0700

    added unnecesary files to the .gitignore

--- STM32CubeIDE/Debug/Application/User/generated/image_alternate_theme_images_widgets_gauge_small_backgrounds_light_filled.d
+++
@@ -1,4 +0,0 @@
-Application/User/generated/image_alternate_theme_images_widgets_gauge_small_backgrounds_light_filled.o: \
- D:/Repos/Brainiac/Brainiac/TouchGFX/generated/images/src/__generated/image_alternate_theme_images_widgets_gauge_small_backgrounds_light_filled.cpp \
- ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
--- STM32CubeIDE/Debug/Drivers/Components/ft5336.d
+++
@@ -1,6 +0,0 @@
-Drivers/Components/ft5336.o: \
- D:/Repos/Brainiac/Brainiac/Drivers/BSP/Components/ft5336/ft5336.c \
- D:/Repos/Brainiac/Brainiac/Drivers/BSP/Components/ft5336/ft5336.h \
- D:/Repos/Brainiac/Brainiac/Drivers/BSP/Components/ft5336/../Common/ts.h
-D:/Repos/Brainiac/Brainiac/Drivers/BSP/Components/ft5336/ft5336.h:
-D:/Repos/Brainiac/Brainiac/Drivers/BSP/Components/ft5336/../Common/ts.h:
